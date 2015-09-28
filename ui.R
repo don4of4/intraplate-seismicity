8e85a8fb0c8225f4a68b961824808e7fdb5308ad
Dean Kroker <dean@deankroker.com>
2015-09-27 22:45:10 -0400

    up to date now

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 
-install.packages("shiny")
+#install.packages("shiny")
 
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
