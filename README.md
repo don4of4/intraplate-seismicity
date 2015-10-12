1ae0f2205e7bf25fa365813ffb72d9ef5ab271fc
Don Scott <me@donaldscott.com>
2015-10-12 15:45:09 -0400

    Patch:  Push all code unrelated to web services.  Closes #5, #7
    Note: Issue #8 is affected by a time issue.  See
    doc/ExceptionReport.docx
    
    Web services push is for event solutions, i.e. #9.

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Catalog sources:
  - ANSS_2013 (file: anss; includes all mag events 2013 until jun 30, 2015)
  - Small_mag (file: small_mag_ISC_75_2012; includes events from 1975 to 2012 with mag < 2.5 or no magnitude determined)
 
+
+Magnitude Conversions:
+ Magnitude conversions where requred on data from ANSS_2013.  This was the methodology used.
+ ![Table 3.3-1](docs/table3.3_1.png)
+ *Where the "dashed line" is the function `f(lat)= -0.45*lon + 3`.*
+ 
+Filtering Parameters Used:
+ - Todo
+ 
--- import.R
+++ import.R
@@ -6,97 +6,67 @@ install.packages("fossil")
 
 library(ggplot2)
 library(maps)
-library(dplyr)
 library(plyr)
+library(dplyr)
 library(fpc)
 library(fossil)
 
-# Utility function:  DO NOT EDIT
-moveme <- function (invec, movecommand) {
-  movecommand <- lapply(strsplit(strsplit(movecommand, ";")[[1]], 
-                                 ",|\\s+"), function(x) x[x != ""])
-  movelist <- lapply(movecommand, function(x) {
-    Where <- x[which(x %in% c("before", "after", "first", 
-                              "last")):length(x)]
-    ToMove <- setdiff(x, Where)
-    list(ToMove, Where)
-  })
-  myVec <- invec
-  for (i in seq_along(movelist)) {
-    temp <- setdiff(myVec, movelist[[i]][[1]])
-    A <- movelist[[i]][[2]][1]
-    if (A %in% c("before", "after")) {
-      ba <- movelist[[i]][[2]][2]
-      if (A == "before") {
-        after <- match(ba, temp) - 1
-      }
-      else if (A == "after") {
-        after <- match(ba, temp)
-      }
-    }
-    else if (A == "first") {
-      after <- 0
-    }
-    else if (A == "last") {
-      after <- length(myVec)
-    }
-    myVec <- append(temp, values = movelist[[i]][[1]], after = after)
-  }
-  myVec
-}
-
-
-
 data.neic <- read.table("data/NEIC_HM_2014.csv", header = TRUE, sep = ",")
+data.neic$src <- 'HM_2014'
+data.neic$declustered <- TRUE
 
 data.anss <- read.table("data/ANSS_2013.csv", header = TRUE, sep = ",")
 colnames(data.anss) <- c("datetime","lat","lon","dp.km","mag","magtype","nbstations", "gap", "distance", "rms", "source", "eventid")
+data.anss$src <- 'ANSS_2013'
+data.anss$declustered <- FALSE
 
 stations.iris <- read.table("data/out_fetchmdata_sept15", header = FALSE, sep = "|")
 colnames(stations.iris) <- c("net","sta","loc","chan","lat","lon","elev","depth","azimuth","dip","instrument","scale","scalefreq","scaleunits","samplerate","start","end")
+data.anss$network <- 'IRIS'
 
-data.small_mag <- read.table("data/small_mag_ISC_75_2012.csv", header = TRUE, sep = ",")
-colnames(data.small_mag) <- c("EVENTID", "AUTHOR", "DATE","TIME", "LAT","LON", "DEPTH","DEPFIX","AUTHOR2","TYPE","MAG", "Other1", "Other2", "Other3")
+data.small_mag <- read.table("data/small_mag_ISC_75_2012.txt", header = TRUE, sep = "\t")
+colnames(data.small_mag) <- c("eventid", "author", "date","time", "lat","lon", "depth","depfix","magauthor","magtype","mag", "magauthor2", "magtype2", "mag2", "magauthor3", "magtype3", "mag3")
+data.small_mag$src <- 'Small_Mag'
+data.small_mag$declustered <- FALSE
+
+
+# Date formatting
+  options(digits.secs=3)
+  data.neic$datetime <- ISOdatetime(data.neic$y, data.neic$m, data.neic$d, data.neic$h, data.neic$m.1, data.neic$s, tz = "")
+  data.anss$datetime <- as.POSIXct(data.anss$datetime, tz = "")
+  stations.iris$start <- strptime(stations.iris$start,format="%Y-%m-%dT%H:%M:%OS")
+  stations.iris$end <- strptime(stations.iris$end,format="%Y-%m-%dT%H:%M:%OS")
+  data.small_mag$datetime <- paste0(data.small_mag$date, "T", data.small_mag$time)
+  #strptime(data.small_mag$datetime, format="%m/%d/%yT%H:%M:%OS")
+  # FIXME: Hours in this data is invalid; most tuples are > 24.  Why?
+
+# Rename lon and lat
+  data.neic$lon <- data.neic$lon.dE.
+  data.neic$lat <- data.neic$lat.dN.
+  data.neic$lon.dE. <- data.neic$lat.dN. <- data.neic$y <- data.neic$m <- data.neic$d <- data.neic$h <- data.neic$m.1 <- data.neic$s <- NULL
+  
 
 # Magnitude to mw
-data.anss$emw <- ifelse(data.anss$magtype == "ML", 0.806*data.anss$mag + 0.633,
-                   ifelse(data.anss$magtype == "Mb", 0.7813*data.anss$mag + 1.5175,
+  # Check event zone.
+  data.anss$f_NE <- ifelse(data.anss$lat > -0.45*data.anss$lon + 3, 1, 0)
+  data.anss$f_1997GSC <- ifelse(data.anss$lat > -0.45*data.anss$lon + 3, 1, 0)
+  data.anss$f_1982NE <- ifelse(data.anss$NE == 1 && data.anss$source != 'GSC' && format(data.anss$datetime, "%Y") < 1982, 1, 0)
+
+  data.anss$emw <- ifelse(data.anss$magtype == "ML", 0.806*data.anss$mag + 0.633,
+                   ifelse(data.anss$magtype == "Mb", data.anss$mag - 0.316 - 0.118*data.anss$f_NE - 0.192*data.anss$f_1997GSC + 0.280*data.anss$f_1982NE,
                    ifelse(data.anss$magtype == "Md", 0.806*data.anss$mag + 0.633,
                    ifelse(data.anss$magtype == "Mx", -1,
                    ifelse(data.anss$magtype == "Mh", -1,
                    ifelse(data.anss$magtype == "Mc",0.806*data.anss$mag + 0.633,
                    ifelse(data.anss$magtype == "Unk", -1, -2)))))))
 
+  # Filter out tuples with unknown mag
+  data.anss <- subset(data.anss, data.anss$emw > 0)
 
-#FIXME: Why do I have bad values?
-data.anss <- subset(data.anss, data.anss$emw > 0)
-
-# Date formatting
-options(digits.secs=3)
-data.neic$datetime <- ISOdatetime(data.neic$y, data.neic$m, data.neic$d, data.neic$h, data.neic$m.1, data.neic$s, tz = "")
-data.anss$datetime <- as.POSIXct(data.anss$datetime, tz = "")
-stations.iris$start <- strptime(stations.iris$start,format="%Y-%m-%dT%H:%M:%OS")
-stations.iris$end <- strptime(stations.iris$end,format="%Y-%m-%dT%H:%M:%OS")
-
-#add miliseconds to format
-
-#op <- options(digits.secs=3)
-#options(op)
-#stations.iris$start <- options(digits.secs=3)
-#stations.iris$end  <- options(digits.secs=3)
-#options(stations.iris$start) #reset options
-#options(stations.iris$end) #reset options
-
-# Rename lon and lat
-data.neic$lon <- data.neic$lon.dE.
-data.neic$lat <- data.neic$lat.dN.
-data.neic$lon.dE. <- data.neic$lat.dN. <- data.neic$y <- data.neic$m <- data.neic$d <- data.neic$h <- data.neic$m.1 <- data.neic$s <- NULL
-
-# Filter based on lat and long
-m <- rbind.fill(data.neic, data.anss)
-m <- dplyr::bind_rows(data.neic, data.anss)
-
-dataset <- subset(m, lat >= 35.5 & lat <= 43.5 & lon <= -71 & lon >= -84 )
+# Filter out based on lat and long
+  m <- rbind.fill(data.neic, data.anss)
+  m <- dplyr::bind_rows(data.neic, data.anss)
+  dataset <- subset(m, lat >= 35.5 & lat <= 43.5 & lon <= -71 & lon >= -84 )
 
 # Calculate distance matrix for the purposes of clustering.
 coordinates  <- data.frame(long=dataset$lon, lat=dataset$lat)
@@ -120,12 +90,8 @@ p <- ggplot() +
   geom_point(size=2, alpha = .7, aes(dataset$lon, dataset$lat, color=dataset$emw)) +
   scale_color_gradient(low="blue", high="red") +
   theme(plot.background = element_rect(fill = 'grey')) +
-  geom_abline(intercept = 3, slope = -.45, color = "blue", size = 1)
+  geom_abline(intercept = 3, slope = -.45, color = "grey", size = 1)
 
 d <- dbscan(dataset, 10,showplot = 2)
 
-#Configurations for data.small_mag
 
-#Remove duplicates
-#smallmag_df <-data.frame(a,b)
-data.small_mag %>% distinct
