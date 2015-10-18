8baa5fc2680bfd7b7ecbde1228fddbe5e2942c0f
Andrea Stiffelman <andrea.stiffelman@gmail.com>
2015-10-18 17:02:12 -0400

    kmeds update 2

--- import.R
+++ import.R
@@ -97,32 +97,3 @@ p <- ggplot() +
 d <- dbscan(dataset, 10,showplot = 2)
 
 
-##K-Mediods Clustering
-#Can use distance matrix above
-
-coordinates2  <- data.frame(long=dataset$lon, lat=dataset$lat)
-dist2  <- earth.dist(df, dist=T)
-
-#data(mvad)
-## Aggregating state sequence
-#aggMvad <- wcAggregateCases(mvad[, 17:86], weights=mvad$weight)
-## Creating state sequence object
-#mvad.seq <- seqdef(mvad[aggMvad$aggIndex, 17:86], weights=aggMvad$aggWeights)
-## Computing Hamming distance between sequence
-#diss <- seqdist(mvad.seq, method="HAM")
-
-## K-Medoids
-kmclust <- wcKMedoids(dist2, 25, weights=NULL, npass = 1, initialclust=25, method="KMedoids", cluster.only = FALSE, debuglevel=0)
-#clust5 <- wcKMedoids(diss, k=5, weights=aggMvad$aggWeights)
-
-## clust5$clustering contains index number of each medoids
-## Those medoids are
-unique(kmclust$clustering)
-
-## Print the medoids sequences
-#print(mvad.seq[unique(clust5$clustering), ], informat="SPS")
-## Some info about the clustering
-print(kmclust)
-## Plot sequences according to clustering solution.
-#seqdplot(mvad.seq, group=clust5$clustering)
-
